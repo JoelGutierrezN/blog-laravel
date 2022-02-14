4d2ff5c5fcc80b56dcb05f682593405952dc7539
JoelGutierrezN <al221910321@gmail.com>
2022-02-13 23:07:58 -0600

    blog uploada

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Editar Articulo</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <form action="{{ route('posts.update', $post)}}" method="POST" enctype="multipart/form-data">
+                        @method('PUT')
+                        @csrf
+
+                        <div class="form-group">
+                            <label>Titulo *</label>
+                            <input type="text" name="title" class="form-control" required value="{{ old('title', $post->title) }}">
+                        </div>
+
+                        <div class="form-group">
+                            <label>Imagen</label>
+                            <input type="file" name="file" class="form-control-file">
+                        </div>
+
+                        <div class="form-group">
+                            <label>Contenido *</label>
+                            <textarea name="body" cols="6" class="form-control" required>{{ old('body', $post->body) }}</textarea>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Contenido embebido</label>
+                            <textarea name="iframe" cols="6" class="form-control">{{ old('iframe', $post->iframe) }}</textarea>
+                        </div>
+
+                        <div class="form-control">
+                            <input type="submit" value="Actualizar" class="btn btn-primary btn-sm">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
